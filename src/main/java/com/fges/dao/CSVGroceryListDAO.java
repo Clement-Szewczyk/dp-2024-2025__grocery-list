56f272e1ae503e7d41f42742d4647521296f6e3d
Clement-Szewczyk <93139573+Clement-Szewczyk@users.noreply.github.com>
2025-04-25 11:56:54 +0200

    CSV refactor

--- src/main/java/com/fges/dao/CSVGroceryListDAO.java
+++ src/main/java/com/fges/dao/CSVGroceryListDAO.java
@@ -3,27 +3,25 @@ package com.fges.dao;
 import java.io.IOException;
 import java.util.List;
 
-import com.fges.CSV;
 import com.fges.Item;
+import com.fges.util.CsvHelper;
 
 public class CSVGroceryListDAO implements GroceryListDAO {
     private final String fileName;
-    private final CSV csvHandler;
 
     public CSVGroceryListDAO(String fileName) {
-
-
         this.fileName = fileName;
-        this.csvHandler = new CSV();
     }
 
     @Override
     public void load(List<Item> groceryList) throws IOException {
-        csvHandler.load(groceryList, fileName);
+        List<Item> loadedItems = CsvHelper.loadFromFile(fileName);
+        groceryList.clear();
+        groceryList.addAll(loadedItems);
     }
 
     @Override
     public void save(List<Item> groceryList) throws IOException {
-        csvHandler.save(groceryList, fileName);
+        CsvHelper.saveToFile(groceryList, fileName);
     }
 }
