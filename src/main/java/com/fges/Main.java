196891aca099da35c2f30e83f9f58f87b5e982ba
Clement-Szewczyk <93139573+Clement-Szewczyk@users.noreply.github.com>
2025-05-17 10:54:37 +0200

    correction main.java

--- src/main/java/com/fges/Main.java
+++ src/main/java/com/fges/Main.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Pour tester le mode web, décommente la ligne suivante :
-        //runWebServer();
-
         // Pour le mode CLI, laisse la ligne suivante :
         System.exit(exec(args));
     }
