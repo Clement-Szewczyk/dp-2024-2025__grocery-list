7a4384c2da9bca7c0af27004718e901bd5c60e14
Clement-Szewczyk <93139573+Clement-Szewczyk@users.noreply.github.com>
2025-05-17 15:05:35 +0200

    main

--- src/main/java/com/fges/Main.java
+++ src/main/java/com/fges/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Pour le mode CLI, laisse la ligne suivante :
         System.exit(exec(args));
     }
 
